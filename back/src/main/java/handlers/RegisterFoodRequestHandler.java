1db380c09058df0b09b28f6c1def30f94ac5e839
Baylor-Lin <Linzetaibaylor521@gmail.com>
2021-02-03 17:23:36 +0800

    Default Changelist

--- back/src/main/java/handlers/RegisterFoodRequestHandler.java
+++ back/src/main/java/handlers/RegisterFoodRequestHandler.java
@@ -6,10 +6,15 @@ import modules.request.RegisterFoodRequest;
 import modules.response.RegisterFoodResponse;
 
 public class RegisterFoodRequestHandler extends MessageHandler<RegisterFoodRequest> {
+    int DBRegisterFood(RegisterFoodRequest food){
+        System.out.println(food.getID());
+        return 1;
+    }
+
     @Override
     public boolean handle(RegisterFoodRequest message) {
         int flag = 0;
-        //flag = RegisterFood(message);数据库写入
+        flag = DBRegisterFood(message);//数据库写入
         MessageManager.sendMessage(new RegisterFoodResponse(flag));
         return false;
     }
