2ef019e5d47e4407fa6e355527fb41e2d3e819c2
liyueyao1015 <3316277393@qq.com>
2021-02-03 20:51:59 +0800

    添加菜品按钮

--- front/src/main/java/controllers/MainPageController.java
+++ front/src/main/java/controllers/MainPageController.java
@@ -3,14 +3,31 @@ package controllers;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 
-public class MainPageController {
+public class MainPageController<Combobox> {
 
     @FXML
-    private Button buttonExample;
+    private Button buttonAdd;
 
     @FXML
-    private void buttonExampleClickEvent() {
-        buttonExample.setText("clicked");
+    private Button button0;
+
+    @FXML
+    private Button button1;
+
+    @FXML
+    private Combobox Combobox;
+
+
+    @FXML
+    private void button0ClickEvent(){
+    }
+
+    @FXML
+    private void button1ClickEvent(){}
+
+    @FXML
+    private void buttonAddClickEvent() {
+        buttonAdd.setText("clicked");
         System.out.println("Hello");
     }
 
