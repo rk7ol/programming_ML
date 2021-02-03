aafa365bc8cffa9acb84cb092c52e553e3da7347
yilinduan <472379433@qq.com>
2021-02-03 16:16:41 +0800

    create RegisterFoodResponseHandler

--- front/src/main/java/handlers/RegisterFoodResponseHandler.java
+++ front/src/main/java/handlers/RegisterFoodResponseHandler.java
@@ -6,6 +6,9 @@ import modules.response.RegisterFoodResponse;
 public class RegisterFoodResponseHandler extends MessageHandler<RegisterFoodResponse> {
     @Override
     public boolean handle(RegisterFoodResponse message) {
+        int flag=message.getFlag();
+        if(flag==1)
+            System.out.println("添加成功！");
         return false;
     }
 }
