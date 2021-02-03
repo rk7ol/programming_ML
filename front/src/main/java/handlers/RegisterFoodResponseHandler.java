459e3fc4d4446d8849466af3b2e347278ace78e9
yilinduan <472379433@qq.com>
2021-02-03 16:09:54 +0800

    create RegisterFoodResponseHandler

--- front/src/main/java/handlers/RegisterFoodResponseHandler.java
+++ front/src/main/java/handlers/RegisterFoodResponseHandler.java
@@ -1,5 +1,11 @@
 package handlers;
 
+import flow.handlers.MessageHandler;
+import modules.response.RegisterFoodResponse;
 
-public class RegisterFoodResponseHandler {
+public class RegisterFoodResponseHandler extends MessageHandler<RegisterFoodResponse> {
+    @Override
+    public boolean handle(RegisterFoodResponse message) {
+        return false;
+    }
 }
