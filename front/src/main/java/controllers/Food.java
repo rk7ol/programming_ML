d84a9c42d2b9fe8271289d33bf101f13d061b82c
liyueyao1015 <3316277393@qq.com>
2021-02-05 23:23:01 +0800

    添加菜品按钮

---
+++ front/src/main/java/controllers/Food.java
@@ -0,0 +1,24 @@
+package controllers;
+
+//添加菜品信息类
+public class Food {
+
+    public String names;
+    public String ways;
+    public String price;
+
+    public String getNames()
+    {
+       // System.out.println("1");
+        return names;
+    }
+
+    public String getPrice() {
+       // System.out.println("1");
+        return price;
+
+    }
+    public String getWays() {
+        return ways;
+    }
+}
--- front/src/main/java/controllers/MainPageController.java
+++ front/src/main/java/controllers/MainPageController.java
@@ -1,10 +1,15 @@
 package controllers;
 
+//import controllers.Food;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
-public class MainPageController<Combobox> {
+
+
+public class MainPageController<Combobox, string> {
+
+
 
     @FXML
     private Button buttonAdd;
@@ -20,25 +25,61 @@ public class MainPageController<Combobox> {
 
 
     @FXML
-  private TextField textfield1;
-
+    private TextField textfield1;
 
     @FXML
-    private void button0ClickEvent() {
+    private TextField textfield2;
+
+    @FXML
+    private TextField textfield3;
+
+    @FXML
+    //确认所选择菜品
+    private void yes() {
     }
 
     @FXML
     private void button1ClickEvent() {
     }
 
+    @FXML
+    //菜品名称
+    private void addaction1()
+    {
+        Food Name = new Food();
+        Name.names = textfield1.getText();//获取当前输入的菜品名称
+        Food food =new Food();
+        food.getNames();
+       // System.out.println(names);
+    }
 
     @FXML
-    private void eventAdd() {
+    //销售方式
+    private  void addaction2() {
+        Food Way=new Food();
+        Way.ways=textfield2.getText();
+        Food food =new Food();
+        food.getWays();
 
     }
 
     @FXML
-    private void addaction() {
+    //价格
+    private void addaction3() {
+        Food Price=new Food();
+        Price.price=textfield3.getText();
+       // System.out.println(price);
+        Food food =new Food();
+        food.getPrice();
     }
 
+    @FXML
+    //添加菜品按钮
+    private void eventAddclick()
+    {
+        addaction1();
+        addaction2();
+        addaction3();
+    }
 }
+
