7b7454e76451302cb491260ab313759da5c2c611
yilinduan <472379433@qq.com>
2021-02-06 19:37:18 +0800

    添加sent

--- front/src/main/java/send/send.java
+++ front/src/main/java/send/send.java
@@ -1,11 +1,13 @@
 package send;
 import flow.MessageManager;
+import modules.request.FoodsRequest;
+import modules.request.RegisterFoodRequest;
 
 import handlers.RegisterFoodResponseHandler;
 import org.apache.avro.Protocol;
 
 public class send {
     public static void sendRegisterFoodRequest(String ID, String name, String method, String price){
-        MessageManager.sendMessage(new RegisterFoodRequest(ID,name,method,Double.parseDouble(price));
+        MessageManager.sendMessage(new RegisterFoodRequest(ID,name,method,Double.parseDouble(price)));
     }
 }
