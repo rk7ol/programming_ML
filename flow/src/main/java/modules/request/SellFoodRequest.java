54731096080d373a25996b731232222cbc0b451d
k7ol <56666912+rk7ol@users.noreply.github.com>
2021-02-27 12:55:26 +0800

    add: SellFoodRequest add parameter (String ID)

--- flow/src/main/java/modules/request/SellFoodRequest.java
+++ flow/src/main/java/modules/request/SellFoodRequest.java
@@ -9,20 +9,25 @@ import org.apache.avro.generic.GenericRecord;
 import java.util.List;
 
 /**
- *  sell foods
- *
- *  List<Food> foods
+ * sell foods
+ * <p>
+ * List<Food> foods
+ * String ID
  */
 public class SellFoodRequest extends Request {
     private Foods foods;
+
+    private String ID;
 
     public List<Food> getFoods() {
         return foods.getContent();
     }
 
-    public SellFoodRequest(Food... foods) {
+
+    public SellFoodRequest(String ID, Food... foods) {
         super("SELL_FOOD_REQUEST");
         this.foods = new Foods(foods);
+        this.ID = ID;
     }
 
     public SellFoodRequest(GenericRecord record) {
@@ -37,6 +42,8 @@ public class SellFoodRequest extends Request {
 
         this.foods = new Foods((GenericRecord) record.get("foods"));
 
+        this.ID = record.get("ID").toString();
+
     }
 
     @Override
@@ -48,6 +55,7 @@ public class SellFoodRequest extends Request {
 
         record.put("foods", foods.serialize());
 
+        record.put("ID", ID);
 
 
         return record;
