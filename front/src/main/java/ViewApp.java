a2c31585921f9003fccb958cddaa15c10ea6cfa8
liyueyao1015 <3316277393@qq.com>
2021-03-01 10:12:35 +0800

    界面

--- front/src/main/java/ViewApp.java
+++ front/src/main/java/ViewApp.java
@@ -6,18 +6,32 @@ import javafx.stage.Stage;
 
 public class ViewApp extends Application {
 
+    Scene scene1;
+    Scene scene2;
+
+    Stage stage;
+
     @Override
     public void start(Stage stage) throws Exception {
 
-        FXMLLoader loader = new FXMLLoader(Starter.class.getResource("views/MainPage.fxml"));
+        this.stage=stage;
 
-        AnchorPane anchorPane = loader.load();
+        FXMLLoader loader1 = new FXMLLoader(Starter.class.getResource("views/MainPage.fxml"));
+        AnchorPane page1 = loader1.load();
+        scene1 = new Scene(page1);
 
-        Scene scene = new Scene(anchorPane);
 
-        stage.setScene(scene);
+        FXMLLoader loader2 = new FXMLLoader(Starter.class.getResource("views/register.fxml"));
+        AnchorPane page2 = loader2.load();
+        scene2 = new Scene(page2);
+
+        stage.setScene(scene2);
 
         stage.show();
 
+    }
+    private void setScene(Scene scene)
+    {
+        stage.setScene(scene);
     }
 }
