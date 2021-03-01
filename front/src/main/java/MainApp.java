fcb3a7d89e5b271680e5e7b5ba2ca58a249e31fe
liyueyao1015 <3316277393@qq.com>
2021-03-01 18:00:38 +0800

    开始界面、结算界面等改动

--- front/src/main/java/MainApp.java
+++ front/src/main/java/MainApp.java
@@ -1,44 +1,8 @@
-import flow.MessageManager;
-import handlers.RegisterFoodResponseHandler;
-import modules.Food;
-import modules.Response;
-import modules.response.RegisterFoodResponse;
 
-import java.util.Random;
 
 public class MainApp {
 
-    public static void main(String[] args) {
-/*
-        MessageManager<Response> messageManager = new MessageManager<>(FoodsResponse.class);
+    public  static void main(String[] args) {
 
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                while (true) {
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-
-                    //create response
-                    int count = new Random().nextInt(5) + 1;
-                    FoodsResponse foodsResponse = new FoodsResponse();
-                    for (int i = 0; i < count; i++)
-                        foodsResponse.getFoods().add(new Food("菜品名", 48));
-
-                    //send message
-                    MessageManager.sendMessage(foodsResponse);
-                }
-
-            }
-        }).start();
-
-        messageManager.addHandler(FoodsResponse.class, new FoodsResponseHandler());
-        messageManager.addHandler(RegisterFoodResponse.class, new RegisterFoodResponseHandler());
-        messageManager.start();
-
-*/
     }
 }
