d60ddc3082a53f5df3674ee97db3c93acd18b942
Baylor-Lin <43720556+Baylor-Lin@users.noreply.github.com>
2021-03-03 23:10:09 +0800

    upload handler

---
+++ AddFoodsRequestHandler.java
@@ -0,0 +1,32 @@
+package handlers;
+
+import Database.OP.KC_Table;
+import Database.OV.KC_OV;
+import flow.MessageManager;
+import flow.handlers.MessageHandler;
+import modules.Food;
+import modules.request.AddFoodsRequest;
+import modules.response.AddFoodsResponse;
+import modules.response.RegisterFoodResponse;
+
+import java.util.List;
+
+public class AddFoodsRequestHandler extends MessageHandler<AddFoodsRequest> {
+    @Override
+    public boolean handle(AddFoodsRequest message) {
+        boolean flag = true;
+        List<Food> foodList = message.getFoods();
+        try {
+            KC_Table kc_table = new KC_Table();
+            for (int i = 0; i < foodList.size(); i++) {
+                KC_OV kc_ov = new KC_OV(foodList.get(i).getID(), message.getID());
+                flag = kc_table.insert(kc_ov);
+                if (flag == false) break;
+            }
+            MessageManager.sendMessage(new AddFoodsResponse("",flag));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return true;
+    }
+}
---
+++ RegisterFoodRequestHandler.java
@@ -0,0 +1,32 @@
+package handlers;
+
+import Database.OP.CP_Table;
+import Database.OV.CP_OV;
+import flow.MessageManager;
+import flow.handlers.MessageHandler;
+import modules.request.RegisterFoodRequest;
+import modules.response.RegisterFoodResponse;
+
+import static handlers.RegisterWindowRequestHandler.getUUID32;
+
+public class RegisterFoodRequestHandler extends MessageHandler<RegisterFoodRequest> {
+    @Override
+    public boolean handle(RegisterFoodRequest message) {
+        boolean flag = false;
+        CP_OV cp_ov=new CP_OV();
+        String ID=getUUID32();
+        cp_ov.setName(message.getName());
+        cp_ov.setWay(message.getMethod());
+        cp_ov.setPrice(message.getPrice());
+        try {
+            CP_Table cp_table=new CP_Table();
+            flag = cp_table.insert(cp_ov);//数据库写入
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        MessageManager.sendMessage(new RegisterFoodResponse("",flag));
+        return true;
+    }
+}
+
+
