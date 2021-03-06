1113e65a90e5de7fdf9111f33c999f70321729a8
yilinduan <472379433@qq.com>
2021-03-06 13:07:47 +0800

    ManageMainApp添加handler和启动界面语句

--- front/src/main/java/ManagerMainApp.java
+++ front/src/main/java/ManagerMainApp.java
@@ -11,18 +11,18 @@ public class ManagerMainApp {
         MessageManager<Response> message = new MessageManager<>();
 
         message.addHandler(DeleteFoodsResponse.class,new DeleteFoodsResponseHandler());
-        //message.addHandler();
-        //message.addHandler(RegisterFoodResponse.class, new RegisterFoodResponseHandler());
-        //message.addHandler(AddFoodsResponse.class, new AddFoodsResponseHandler());
+        message.addHandler(RegisterFoodResponse.class, new RegisterFoodResponseHandler());
+        message.addHandler(AddFoodsResponse.class, new AddFoodsResponseHandler());
         //message.addHandler(RegisterWindowResponse.class, new RegisterWindowResponseHandler());
         //message.addHandler(SellFoodResponse.class, new SellFoodResponseHandler());
         //message.addHandler(SettleResponse.class, new SettleResponseHandler());
-        //message.addHandler(ShowAllFoodsResponse.class, new ShowAllFoodsResponseHandler());
+        message.addHandler(ShowAllFoodsResponse.class, new ShowAllFoodsResponseHandler());
         //message.addHandler(ShowWindowFoodsResponse.class, new ShowWindowFoodsResponseHandler());
 
         message.start();
 
-        Application.launch(UserViewApp.class);
+        Application.launch(ManagerViewApp.class);
 
+        message.stop();
     }
 }
