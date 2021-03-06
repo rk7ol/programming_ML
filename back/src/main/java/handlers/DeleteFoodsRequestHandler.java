c07d7b304ddac166bdd69179874a01ddee572da9
Baylor-Lin <43720556+Baylor-Lin@users.noreply.github.com>
2021-03-06 13:57:49 +0800

    Add files via upload

--- back/src/main/java/handlers/DeleteFoodsRequestHandler.java
+++ back/src/main/java/handlers/DeleteFoodsRequestHandler.java
@@ -3,6 +3,7 @@ package handlers;
 import Database.DB;
 import Database.OPFactory;
 import Database.OV.CP_OV;
+import Database.OV.KC_OV;
 import flow.MessageManager;
 import flow.handlers.MessageHandler;
 import modules.Food;
@@ -18,9 +19,13 @@ public class DeleteFoodsRequestHandler extends MessageHandler<DeleteFoodsRequest
         Boolean flag=false;
         try {
             DB cp_table=opFactory.createTable("CP_Table");
+            DB kc_table=opFactory.createTable("KC_Table");
             List<Food> foodList= message.getFoods();
             for (int i = 0; i < foodList.size(); i++) {
+                KC_OV kc_ov=new KC_OV();
+                kc_ov.setC_ID(foodList.get(i).getID());
                 CP_OV cp_ov=new CP_OV(foodList.get(i).getID(),foodList.get(i).getName(),foodList.get(i).getMethod(),foodList.get(i).getPrice());
+                kc_table.delete(kc_ov);
                 flag=cp_table.delete(cp_ov);
             }
             MessageManager.sendMessage(new DeleteFoodsResponse(message.getSession(),flag));
